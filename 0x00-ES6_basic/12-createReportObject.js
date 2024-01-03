e32c9e0f393a8e96cf1eb0943b956a647f898e38
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-03 17:15:14 +0200

    Adfdd task 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(list) {
+      return Object.keys(list).length;
+    },
+  };
+}
