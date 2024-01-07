21c8f3dc92a53ea40470f5f6875d8335c9295d9b
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-07 17:42:25 +0200

    Add task 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,4 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) { throw new Error('cannot divide by 0'); }
-    return numerator / denominator;
-  } catch (err) {
-    return err;
-  }
+  if (denominator === 0) { throw new Error('cannot divide by 0'); }
+  return numerator / denominator;
 }
