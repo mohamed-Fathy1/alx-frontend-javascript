eae04f179002d8e266a6524a8de489c37e12a990
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-07 16:51:26 +0200

    Add task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
+/* eslint-disable no-unused-vars */
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(),
+    uploadPhoto(),
+  ]);
+}
