720daf1c0658075ac93bb7baac69effde565f7fb
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-07 15:38:02 +0200

    Add task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,12 @@
+import { uploadPhoto, createUser } from './utils.js';
 
+export default function handleProfileSignup () {
+  Promise.all([
+    uploadPhoto(),
+    createUser()
+  ])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName} `);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
