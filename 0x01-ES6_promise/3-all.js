47c37d487d1ce1a85c615d3e00a6dfbb37c3cf05
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-07 16:06:03 +0200

    Add tak 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photos = uploadPhoto();
-  const users = createUser();
-
   return Promise.all([
-    photos,
-    users,
-  ]).then((data) => {
-    console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName} `);
-  })
-    .catch(() => { console.log('Signup system offline'); });
+    uploadPhoto(),
+    createUser(),
+  ])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
