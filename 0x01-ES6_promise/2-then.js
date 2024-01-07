1ea8646101194f711a8adf0d25a49fd4d041c2a5
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-07 14:51:40 +0200

    Add taask 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else { reject(); }
-  })
-    .then((data) => data)
+  return promise
+    .then(({ status: 200, body: 'success' }))
     .catch(Error())
     .finally(console.log('Got a response from the API'));
 }
