9860ae1e9d5e3280f3d0ea0d97155327861e0393
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-07 18:03:08 +0200

    Add task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  try {
-    const prom1 = await uploadPhoto();
-    const prom2 = await createUser();
-
-    return ({ photo: prom1, user: prom2 });
-  } catch (error) {
-    return ({ photo: null, user: null });
-  }
+  return {
+    photo: await uploadPhoto(),
+    user: await createUser(),
+  };
 }
