931d40962352b834674700ea27b42ba859681474
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-07 17:55:48 +0200

    Add task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (err) {
-    queue.push(err);
+    queue.push(`${err}`.split('\n')[0]);
   } finally {
     queue.push('Guardrail was processed');
   }
