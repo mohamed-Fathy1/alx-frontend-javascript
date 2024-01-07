c6f6ef29c3dda6bd58b39c22672bf6a56bd52034
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-07 17:40:59 +0200

    Add taak 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,8 @@
-
+export default function divideFunction(numerator, denominator) {
+  try {
+    if (denominator === 0) { throw new Error('cannot divide by 0'); }
+    return numerator / denominator;
+  } catch (err) {
+    return err;
+  }
+}
