940eb6a11ac129f9b26c72f15fccb8e106b044f1
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-07 16:59:59 +0200

    Add task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-/* eslint-disable no-unused-vars */
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
-    signUpUser(),
-    uploadPhoto(),
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
   ]);
+  // .then((data) => {
+  //   const result = [];
+  //   data.forEach((item) => {
+  //     if (item.status === 'fulfilled') {
+  //       console.log(item.value);
+  //       result.push(item.value);
+  //     } else {
+  //       console.log(item.reason);
+  //       result.push(item.reason);
+  //     }
+  //   });
+  //   return result;
+  // });
 }
