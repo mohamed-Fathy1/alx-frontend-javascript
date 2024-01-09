2cd9b99b08d30b9cf931652b9e3264a4d6e68568
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-10 00:28:33 +0200

    Add task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-export default function getStudentIds (arr) {
+export default function getStudentIds(arr) {
   if (!Array.isArray(arr)) {
     return [];
   }
-  return arr.map(item => item.id);
+  return arr.map((item) => item.id);
 }
