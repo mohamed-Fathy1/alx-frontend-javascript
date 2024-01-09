f046d8facccfd4b309b52c5145eb0b7efd313049
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-09 21:22:20 +0200

    Add taas k5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') { throw new TypeError('sqft must be a number'); }
+    this._sqft = sqft;
+  }
 
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') { throw new TypeError('sqft must be a number'); }
+
+    this._sqft = value;
+  }
+}
