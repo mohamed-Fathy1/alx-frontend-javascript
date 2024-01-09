480edc47757a2b72e7abf039015de070d80e5690
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-09 20:54:42 +0200

    Add task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,50 @@
+import Currency from './3-currency';
 
+export default class Pricing {
+  constructor (amount, currency) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw TypeError('Currency must be a Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount () {
+    return this._amount;
+  }
+
+  get currency () {
+    return this._currency;
+  }
+
+  set amount (amount) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  set currency (currency) {
+    if (!(currency instanceof Currency)) {
+      throw TypeError('Currency must be a Currency');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice () {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice (amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw TypeError('ConversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
