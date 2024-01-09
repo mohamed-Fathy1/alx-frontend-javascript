5002b9139e468d45d75f5fff1228db84fae31839
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-09 20:44:53 +0200

    Add task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string' || typeof name !== 'string') { throw new TypeError('Name must be a string'); }
 
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') { throw new TypeError('Code must be a string'); }
+    this._code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') { throw new TypeError('Name must be a string'); }
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
