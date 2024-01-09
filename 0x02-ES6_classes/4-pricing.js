7e109bd2532536fd0f296e38a4f209dab8366d0e
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-09 20:55:40 +0200

    Add tasak 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,7 +1,7 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  constructor (amount, currency) {
+  constructor(amount, currency) {
     if (typeof amount !== 'number') {
       throw TypeError('Amount must be a number');
     }
@@ -12,33 +12,33 @@ export default class Pricing {
     this._currency = currency;
   }
 
-  get amount () {
+  get amount() {
     return this._amount;
   }
 
-  get currency () {
+  get currency() {
     return this._currency;
   }
 
-  set amount (amount) {
+  set amount(amount) {
     if (typeof amount !== 'number') {
       throw TypeError('Amount must be a number');
     }
     this._amount = amount;
   }
 
-  set currency (currency) {
+  set currency(currency) {
     if (!(currency instanceof Currency)) {
       throw TypeError('Currency must be a Currency');
     }
     this._currency = currency;
   }
 
-  displayFullPrice () {
+  displayFullPrice() {
     return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 
-  static convertPrice (amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
     if (typeof amount !== 'number') {
       throw TypeError('Amount must be a number');
     }
