0a3e9a912a7f8b49505b39d9dc97e37f55da1e56
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-09 22:40:36 +0200

    Add task 8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,15 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    } if (hint === 'number') {
+      return this._size;
+    }
+    return this._size;
+  }
+}
