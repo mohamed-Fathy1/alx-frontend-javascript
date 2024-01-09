1e41339c1b7dcb1401f3e297b3f501e9924f93bc
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-09 20:02:23 +0200

    Add task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,1 +1,6 @@
-
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    if (typeof maxStudentsSize !== 'number') { throw new TypeError('maxStudentsSize must be of type Number'); }
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
