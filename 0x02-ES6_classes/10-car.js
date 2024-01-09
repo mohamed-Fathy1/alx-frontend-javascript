1dc99b1631f4eb8f44ac78f094b5c229a8f22f5c
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-09 23:23:14 +0200

    Add task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,17 +1,17 @@
 export default class Car {
-  constructor(brand, motor, color) {
+  constructor (brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  static get [Symbol.species]() {
+  static get [Symbol.species] () {
     return this;
   }
 
-  cloneCar() {
-    return new this.constructor[Symbol.species](
-      this._brand, this._motor, this._color,
+  cloneCar () {
+    this.constructor[Symbol.species]{
+        this._brand, this._motor, this._color,
     );
   }
 }
