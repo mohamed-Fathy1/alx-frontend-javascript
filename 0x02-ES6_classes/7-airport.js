2c0a1670a9869e05dc1bd6106775ac8879760fb9
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-09 22:22:07 +0200

    Add taask 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,13 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string' || typeof code !== 'string') {
+      throw new TypeError('Name and code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
 
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
