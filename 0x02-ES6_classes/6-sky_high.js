37f56439dd499937c31b4c80500dabad03008231
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-09 21:30:28 +0200

    Add task 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -18,6 +18,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
