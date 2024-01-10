e5bb8e5e05282ca0e236eb9b5fe4ebc5a2fb7fa5
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-10 09:17:25 +0200

    Add task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
-export default function updateStudentGradeByCity (students, city, newGrade) {
-  return students.filter(student => student.location === city)
-    .map(item => {
-      /* eslint-disable */
-      item.grade = newGrade.find((grade) => grade.studentId === item.id)?.grade ?? 'N/A';
+export default function updateStudentGradeByCity(students, city, newGrade) {
+  return students.filter((student) => student.location === city)
+    .map((item) => {
+      /* eslint-disable no-param-reassign */
+      if (!newGrade.some((grade) => grade.studentId === item.id)) { item.grade = 'N/A'; } else { item.grade = newGrade.find((grade) => grade.studentId === item.id).grade; }
       return item;
     });
 }
