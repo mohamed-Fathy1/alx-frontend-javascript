94c2ac13e734ae2c7a529f36b278683a359d72aa
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-13 17:40:46 +0200

    Add tak 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,12 @@
+const weakMap = new WeakMap();
 
+function queryAPI(ep) {
+  if (!weakMap.has(ep)) { weakMap.set(ep, 0); }
+
+  let numberOfApiCalls = (weakMap.get(ep));
+  numberOfApiCalls += 1;
+  weakMap.set(ep, numberOfApiCalls);
+  if (numberOfApiCalls >= 5) { throw new Error('Endpoint load is high'); }
+}
+
+export { queryAPI, weakMap };
