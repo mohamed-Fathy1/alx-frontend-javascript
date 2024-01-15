db3fb62c1158df34da2bf2304d4eb6fe81a5b3eb
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-15 13:36:32 +0200

    Add task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -44,3 +44,33 @@ const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
     return `${firstName[0].toUpperCase()}. ${lastName}`
 };
 console.log(printTeacher(director1.firstName, director1.lastName));
+
+
+// Task 4
+interface Student{
+    firstName: string,
+    lastName: string,
+    workOnHomework(): string,
+    displayName(): string
+}
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+const student1 = new StudentClass('John', 'Doe');
+console.log(student1.workOnHomework());
+console.log(student1.displayName());
